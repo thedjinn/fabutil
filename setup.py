cd28d0569c0446a3fcb82b32fef4f9fed29a17c0
Emil Loer <emil@koffietijd.net>
2011-09-26 09:26:24 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="fabutil",
+    version="0.1.0",
+    description="Fabric output beautification and deployment power tools",
+    author="Emil Loer",
+    author_email="emil@koffietijd.net",
+    url="http://emilloer.com",
+    packages=["fabutil"],
+    license="MIT"
+)
