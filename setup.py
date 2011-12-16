d444c529cf348cd521cd290152d6dfbff6e0b8b0
Emil Loer <emil@koffietijd.net>
2011-12-16 13:37:00 +0100

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="fabutil",
-    version="0.1.0",
+    version="0.1.1",
     description="Fabric output beautification and deployment power tools",
     author="Emil Loer",
     author_email="emil@koffietijd.net",
